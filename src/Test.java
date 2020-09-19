8a418a2dde7ba40a49922d57fed14f28020c2c5d
ouhamzalhss <ouhamza.web.pro@gmail.com>
2020-09-19 20:22:56 +0100

    update

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Lhouceine OUHAMZA
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        float n = 1.2e3f;
+        System.out.println(n);
+    }
+
+}
+
